91f1b6db82c85eee2b9baff39d6c74c5486287d4
Krutika44 <shirsatkrutika4@gmail.com>
2023-07-08 16:41:17 +0530

    commit

---
+++ src/components/GameCircle.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Game.css';
+
+const GameCircle = ({id,children, onCircleClicked, className}) => {
+
+  return (
+      <div className={`gameCircle ${className}`} onClick={() => onCircleClicked(id)}>
+        {children}
+      </div>
+  )
+}
+
+export default GameCircle
